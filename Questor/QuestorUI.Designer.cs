b54bfc06f7ecb9528af484c327a2328c50d3c98d
ISeeDEDPpl <bgates256@gmail.com>
2012-10-09 21:50:42 -0500

    Change the QuestorUI back to its default size when starting.

--- Questor/QuestorUI.Designer.cs
+++ Questor/QuestorUI.Designer.cs
@@ -5781,7 +5781,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(711, 408);
+            this.ClientSize = new System.Drawing.Size(362, 124);
             this.Controls.Add(this.label26);
             this.Controls.Add(this.label25);
             this.Controls.Add(this.buttonQuestormanager);

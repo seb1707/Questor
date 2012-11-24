612345b98536eff462a15ada8bf900821cd740ee
ISeeDEDPpl <bgates256@gmail.com>
2012-11-23 20:07:20 -0600

    Start with Questor GUI not yet expanded

--- Questor/QuestorUI.Designer.cs
+++ Questor/QuestorUI.Designer.cs
@@ -5827,7 +5827,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(702, 409);
+            this.ClientSize = new System.Drawing.Size(356, 96);
             this.Controls.Add(this.label26);
             this.Controls.Add(this.label25);
             this.Controls.Add(this.buttonQuestormanager);

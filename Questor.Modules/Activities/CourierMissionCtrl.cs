64e35483cb41a1d55344efa3d2a816282ec67c65
ISeeDEDPpl <bgates256@gmail.com>
2012-12-19 19:40:11 -0600

    Fix for Pot and Kettle 3 of 5, courier mission

--- Questor.Modules/Activities/CourierMissionCtrl.cs
+++ Questor.Modules/Activities/CourierMissionCtrl.cs
@@ -72,7 +72,7 @@ namespace Questor.Modules.Activities
                     missionItem = "Amarr Light Marines";
                     break;
 
-                case "Pot and Kettle (3 of 5)":                       //lvl4 courier
+                case "Pot and Kettle - Delivery (3 of 5)":                       //lvl4 courier
                     missionItem = "Large EMP Smartbomb I";
                     break;
 

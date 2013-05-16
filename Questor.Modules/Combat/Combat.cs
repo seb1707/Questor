269ebff807bf6179082bfb7685dac20bb611ee4e
Alexej Haak <kontakt@alexej-haak.de>
2013-05-16 14:42:31 +0200

    This change was wrong

--- Questor.Modules/Combat/Combat.cs
+++ Questor.Modules/Combat/Combat.cs
@@ -1048,7 +1048,7 @@ namespace Questor.Modules.Combat
 
             if (Cache.Instance.PreferredDroneTarget != null && (Cache.Instance.UseDrones != null && Cache.Instance.UseDrones)) 
             {
-                if (!Cache.Instance.PreferredDroneTarget.IsLargeCollidable && !Cache.Instance.PreferredDroneTarget.IsValid)
+                if (!Cache.Instance.PreferredDroneTarget.IsLargeCollidable && !Cache.Instance.DronePriorityTargets.Contains(Cache.Instance.PreferredDroneTarget))
                 {
                     Cache.Instance.PreferredDroneTarget = null;
                     return;

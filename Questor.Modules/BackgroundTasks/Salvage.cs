5b4504651403ba15157b0cf3c91fb720547bc85a
ISeeDEDPpl <bgates256@gmail.com>
2013-05-19 20:17:06 -0500

    If you are speed tanking a battleship (wtf!) then assume it is ok to loot... BSs are not fragile snowflakes like smaller ships might be.

--- Questor.Modules/BackgroundTasks/Salvage.cs
+++ Questor.Modules/BackgroundTasks/Salvage.cs
@@ -485,7 +485,7 @@ namespace Questor.Modules.BackgroundTasks
                 }
 
                 // Don't even try to open a wreck if you are speed tanking and you are not processing a loot action
-                if (Settings.Instance.SpeedTank && Cache.Instance.OpenWrecks == false)
+                if (Settings.Instance.SpeedTank && !Cache.Instance.MyShipEntity.IsBattleship && Cache.Instance.OpenWrecks == false)
                 {
                     if (Settings.Instance.DebugLootWrecks) Logging.Log("Salvage.LootWrecks", "SpeedTank is true and OpenWrecks is false [" + containerEntity.Id + "]", Logging.White);
                     continue;

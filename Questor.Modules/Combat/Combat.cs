2a6b63787f021d8c507b43423d246f2fa23ec3ba
Nico RIP <robdeal79@gmail.com>
2013-05-19 23:19:41 -0400

    Null Ref fix....oops!

--- Questor.Modules/Combat/Combat.cs
+++ Questor.Modules/Combat/Combat.cs
@@ -1453,7 +1453,9 @@ namespace Questor.Modules.Combat
                         _States.CurrentCombatState = CombatState.CheckTargets;
 
                         //lets at the least make sure we have a fresh entity this frame to check against so we arent trying to navigate to things that no longer exist
-                        EntityCache killTarget = Cache.Instance.Entities.Where(t => t.Id == Cache.Instance.PreferredPrimaryWeaponTarget.Id && t.IsValid).FirstOrDefault();
+                        EntityCache killTarget = null;
+                        if(Cache.Instance.PreferredPrimaryWeaponTarget != null)
+                            Cache.Instance.Entities.Where(t => t.Id == Cache.Instance.PreferredPrimaryWeaponTarget.Id && t.IsValid).FirstOrDefault();
 
                         if (killTarget != null)
                         {

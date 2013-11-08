fe47feadeeaa58ef3ec3247d5f4b63e41efa0b63
ISeeDEDPpl <bgates256@gmail.com>
2013-11-07 22:23:58 -0600

    Make sure we are choosing a killTarget that is within weapons range.

--- Questor.Modules/Combat/Combat.cs
+++ Questor.Modules/Combat/Combat.cs
@@ -1890,7 +1890,7 @@ namespace Questor.Modules.Combat
                         {
                             if (Cache.Instance.Targets.Any(t => t.Id == Cache.Instance.PreferredPrimaryWeaponTarget.Id))
                             {
-                                killTarget = Cache.Instance.Targets.FirstOrDefault(t => t.Id == Cache.Instance.PreferredPrimaryWeaponTarget.Id);    
+                                killTarget = Cache.Instance.Targets.FirstOrDefault(t => t.Id == Cache.Instance.PreferredPrimaryWeaponTarget.Id && t.Distance < Cache.Instance.MaxRange);    
                             }
                             else
                             {
@@ -1904,7 +1904,7 @@ namespace Questor.Modules.Combat
                         {
                             if (Cache.Instance.Targets.Any(i => !i.IsContainer && !i.IsBadIdea))
                             {
-                                killTarget = Cache.Instance.Targets.Where(i => !i.IsContainer && !i.IsBadIdea && i.Distance < Cache.Instance.WeaponRange).OrderByDescending(i => i.IsInOptimalRange).ThenByDescending(i => i.IsCorrectSizeForMyWeapons).ThenBy(i => i.Distance).FirstOrDefault();
+                                killTarget = Cache.Instance.Targets.Where(i => !i.IsContainer && !i.IsBadIdea && i.Distance < Cache.Instance.MaxRange).OrderByDescending(i => i.IsInOptimalRange).ThenByDescending(i => i.IsCorrectSizeForMyWeapons).ThenBy(i => i.Distance).FirstOrDefault();
                             }
                         }
 

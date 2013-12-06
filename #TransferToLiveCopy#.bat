e62acaccaf4a30ed2518cc5bdff56601f1a3939a
duketwo <seviers@gmail.com>
2013-12-06 12:51:18 +0000

    Update #TransferToLiveCopy#.bat
    allows to test a new questor build without quitting other instances

--- #TransferToLiveCopy#.bat
+++ #TransferToLiveCopy#.bat
@@ -76,6 +76,17 @@ copy /y ".\DirectEVE\easyhook*.*" ".\output\"
 copy /y ".\output\*.pdb" "%innerspacedotnetdirectory%"
 @Echo off
 @Echo.
+@Echo Rename prior Quester.exe to Questor.exe.random.questorold
+ren "%innerspacedotnetdirectory%\Questor.exe" "Questor.exe.%Random%.questorold"
+@Echo off
+@Echo.
+@Echo Rename prior Questor.Modules.dll to Questor.Modules.dll.random.questorold
+ren "%innerspacedotnetdirectory%\Questor.Modules.dll" "Questor.Modules.dll.%Random%.questorold"
+@Echo off
+@Echo.@Echo Trying to delete all .questorold files
+Del /Q "%innerspacedotnetdirectory%\*.questorold"
+@Echo off
+@Echo.
 @Echo Starting to copy DLL files from [.\output\*.dll] to [%innerspacedotnetdirectory%]
 @Echo on
 copy /y ".\output\*.dll" "%innerspacedotnetdirectory%"

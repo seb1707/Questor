3e558d91889d421674886d6ddf95df7ffa195f72
ISeeDEDPpl <bgates256@gmail.com>
2014-02-23 05:17:27 -0600

    increase the reload delay

--- Questor.Modules/Lookup/Time.cs
+++ Questor.Modules/Lookup/Time.cs
@@ -49,7 +49,7 @@ namespace Questor.Modules.Lookup
         public int FittingWindowLoadFittingDelay_seconds = 7;               // We can ask the fitting to be loaded using the fitting window, but we cant know it is done, thus this delay, units: seconds. Default is 10
         public int WaitforItemstoMove_seconds = 1;                          // Arm state: wait for items to move, units: seconds. Default is 5
         public int CheckLocalDelay_seconds = 5;                             // Local Check for bad standings pilots, delay between checks, units: seconds. Default is 5
-        public int ReloadWeaponDelayBeforeUsable_seconds = 12;              // Delay after reloading before that module is usable again (non-energy weapons), units: seconds. Default is 12
+        public int ReloadWeaponDelayBeforeUsable_seconds = 18;              // Delay after reloading before that module is usable again (non-energy weapons), units: seconds. Default is 12
         public int BookmarkPocketRetryDelay_seconds = 20;                   // When checking to see if a bookmark needs to be made in a pocket for after mission salvaging this is the delay between retries, units: seconds. Default is 20
         public int NoGateFoundRetryDelay_seconds = 30;                      // no gate found on grid when executing the activate action, wait this long to see if it appears (lag), units: seconds. Default is 30
         public int AlignDelay_minutes = 2;                                  // Delay between the last align command and the next, units: minutes. Default is 2

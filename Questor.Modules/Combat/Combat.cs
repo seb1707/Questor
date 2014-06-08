105a6c0552264c82114a54d055eb0d4cd9de353d
ISeeDEDPpl <bgates256@gmail.com>
2014-06-08 10:14:25 -0500

    Do not attempt to activate bastion during PVP if we have no ammo! In that case we probably want to flee

--- Questor.Modules/Combat/Combat.cs
+++ Questor.Modules/Combat/Combat.cs
@@ -1104,7 +1104,7 @@ namespace Questor.Modules.Combat
                 Time.Instance.NextBastionModeDeactivate = DateTime.UtcNow.AddSeconds(-2);
             }
 
-            if (Cache.Instance.PotentialCombatTargets.Any(e => e.Distance < Cache.Instance.WeaponRange && e.IsPlayer && e.IsTargetedBy && e.IsAttacking))
+            if (Cache.Instance.PotentialCombatTargets.Any(e => e.Distance < Cache.Instance.WeaponRange && e.IsPlayer && e.IsTargetedBy && e.IsAttacking) && _States.CurrentCombatState != CombatState.OutOfAmmo)
             {
                 if (Settings.Instance.DebugActivateBastion) Logging.Log("ActivateBastion", "We are being attacked by a player we should activate bastion", Logging.Debug);
                 activate = true;

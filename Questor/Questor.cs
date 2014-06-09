ddcce6285e00c19b5cb91b12d7395e9bbc87623e
ISeeDEDPpl <bgates256@gmail.com>
2014-06-09 01:16:07 -0500

    compile fix, missing bits from the last commit.

--- Questor/Questor.cs
+++ Questor/Questor.cs
@@ -196,7 +196,7 @@ namespace Questor
                     _runOnceAfterStartupalreadyProcessed = true;
 
                     Cache.Instance.IterateShipTargetValues("RunOnceAfterStartup");  // populates ship target values from an XML
-                    Cache.Instance.IterateInvTypes("RunOnceAfterStartup");          // populates the prices of items (cant we use prices from the game now?!)
+                    //Cache.Instance.IterateInvTypes("RunOnceAfterStartup");          // populates the prices of items (cant we use prices from the game now?!)
                     Cache.Instance.IterateUnloadLootTheseItemsAreLootItems("RunOnceAfterStartup");       // populates the list of items we never want in our local cargo (used mainly in unloadloot)
 
                     if (Settings.Instance.UseInnerspace)

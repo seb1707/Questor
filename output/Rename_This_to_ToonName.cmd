39a9c50c2cb482432a7fd4dcd3948c5a00be266a
ISeeDEDPpl <bgates256@gmail.com>
2014-06-22 10:26:00 -0500

    Rudimentary Adapteve integration and standalone support (no innerspace needed), this will need to be integrated into duketwos launcher before it will be "esay" to use. Not that this iteration is hard, but its not easy either. Note: you CAN continue to use innerspace as before.

---
+++ output/Rename_This_to_ToonName.cmd
@@ -0,0 +1,17 @@
+@Echo off
+cls
+
+Set InjectorEXE=Adapteve.exe
+Set iniFile=setting.ini
+::
+:: we eventually need to also have things like AccountName, AccountPassword, CharacterName (the login info that is in Schedules.XML)
+::
+
+Echo Injector Details Are as Follows: 
+Echo.
+Echo InjectorEXE - This does the actual injection [ %InjectorEXE% ] 
+Echo iniFile                                      [ %iniFile% ]
+Echo.
+Echo Running [ %injectorexe% %inifile% ]
+%injectorexe% %inifile%
+pause

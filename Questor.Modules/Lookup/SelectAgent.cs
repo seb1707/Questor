92b08074e7c03044c735544a06285388f25c445c
ISeeDEDPpl <bgates256@gmail.com>
2014-07-17 22:10:00 -0500

    Add try/catch blocks to the decline timer csv stuff

--- Questor.Modules/Lookup/SelectAgent.cs
+++ Questor.Modules/Lookup/SelectAgent.cs
@@ -25,45 +25,84 @@
 
             private void _loadFromCacheFile()
             {
-                _timers = new Dictionary<string, DateTime>();
-
-                if (File.Exists(_agentMissionDeclineTimesFilePath))
+                try
                 {
-                    Logging.Log("AgentsDeclineTimes", String.Format("Loading agents decline times from cache file : {0}", _agentMissionDeclineTimesFilePath), Logging.White);
+                    _timers = new Dictionary<string, DateTime>();
 
-                    System.IO.StreamReader file = new System.IO.StreamReader(_agentMissionDeclineTimesFilePath);
-
-                    string line;
-                    // Read and display lines from the file until the end of 
-                    // the file is reached.
-                    while ((line = file.ReadLine()) != null)
+                    if (File.Exists(_agentMissionDeclineTimesFilePath))
                     {
-                        string[] lineValues = line.Split(';');
+                        Logging.Log("AgentsDeclineTimes", String.Format("Loading agents decline times from cache file : {0}", _agentMissionDeclineTimesFilePath), Logging.White);
 
-                        string agentName = lineValues[0];
-                        DateTime declineTime = new DateTime(long.Parse(lineValues[1]));
+                        System.IO.StreamReader file = new System.IO.StreamReader(_agentMissionDeclineTimesFilePath);
 
-                        _timers.Add(agentName, declineTime);
+                        string line;
+                        // Read and display lines from the file until the end of 
+                        // the file is reached.
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            string[] lineValues = line.Split(';');
 
-                        Logging.Log("AgentsDeclineTimes", String.Format("Found agent decline time in cache file : {0}, {1}", agentName, declineTime.ToString()), Logging.White);
+                            string agentName = lineValues[0];
+                            DateTime declineTime = new DateTime(long.Parse(lineValues[1]));
+
+                            _timers.Add(agentName, declineTime);
+
+                            Logging.Log("AgentsDeclineTimes", String.Format("Found agent decline time in cache file : {0}, {1}", agentName, declineTime.ToString()), Logging.White);
+                        }
+                    }
+                    else
+                    {
+                        Logging.Log("AgentsDeclineTimes", String.Format("No decline times loaded because cache file does not exist : {0}", _agentMissionDeclineTimesFilePath), Logging.White);
                     }
                 }
-                else
+                catch (Exception exception)
                 {
-                    Logging.Log("AgentsDeclineTimes", String.Format("No decline times loaded because cache file does not exist : {0}", _agentMissionDeclineTimesFilePath), Logging.White);
+                    Logging.Log("AgentsDeclineTimes", "Exception [" + exception + "]", Logging.Teal);
                 }
             }
 
             private void _writeToCacheFile()
             {
-                Logging.Log("AgentsDeclineTimes", String.Format("Writing agents decline times to cache file : {0}", _agentMissionDeclineTimesFilePath), Logging.White);
+                try
+                {
+                    StreamWriter file = null;
+                    Logging.Log("AgentsDeclineTimes", String.Format("Writing agents decline times to cache file : {0}", _agentMissionDeclineTimesFilePath), Logging.White);
 
-                System.IO.StreamWriter file = new System.IO.StreamWriter(_agentMissionDeclineTimesFilePath);
-
-                foreach (var entry in _timers)
-                    file.WriteLine("{0};{1};{2};{3}", entry.Key, entry.Value.Ticks, entry.Value.ToString(), DateTime.UtcNow.ToString()); 
-
-                file.Close();
+                    try
+                    {
+                        file = new StreamWriter(_agentMissionDeclineTimesFilePath);
+                        if (file != null)
+                        {
+                            foreach (var entry in _timers)
+                            {
+                                file.WriteLine("{0};{1};{2};{3}", entry.Key, entry.Value.Ticks, entry.Value.ToString(), DateTime.UtcNow.ToString());
+                            }    
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        //Logging.Log("AgentsDeclineTimes", "IOException: [" + ex + "]", Logging.Debug);
+                    }
+                    catch (Exception ex)
+                    {
+                        Logging.Log("AgentsDeclineTimes", "Exception: [" + ex + "]", Logging.Debug);
+                    
+                    }
+                    finally
+                    {
+                        if (file != null)
+                        {
+                            file.Flush();
+                            file.Close();
+                            file.Dispose();
+                        }
+                    }                    
+                    
+                }
+                catch (Exception exception)
+                {
+                    Logging.Log("AgentsDeclineTimes", "Exception [" + exception + "]", Logging.Teal);
+                }
             }
                         
             public DateTime getDeclineTimer(string agentName)
